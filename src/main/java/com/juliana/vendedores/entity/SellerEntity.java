bd8012e0d8ae7a49e62d63f855641ac64f8f696b
JulianaOAguilar <julianaoaguilar16@gmail.com>
2026-06-16 13:52:52 -0300

    CLoses #2 - Entity Seller
     added JPA Mapping

---
+++ src/main/java/com/juliana/vendedores/entity/SellerEntity.java
@@ -0,0 +1,83 @@
+package com.juliana.vendedores.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "TBL_SELLER") 
+
+public class SellerEntity {
+
+    @Id // definir ID como PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Long id;
+    private String name;
+    private Double salary;
+    private Double bonus;
+
+    public enum Gender {
+        MALE,
+        FEMALE
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(Double bonus) {
+        this.bonus = bonus;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SellerEntity other = (SellerEntity) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
