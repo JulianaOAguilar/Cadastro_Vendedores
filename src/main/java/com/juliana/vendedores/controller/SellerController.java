d1fe7aaa78fe72b4cf61a3070374b2499b00b6df
JulianaOAguilar <julianaoaguilar16@gmail.com>
2026-06-22 16:24:49 -0300

    closes #10 - Modal and Service

--- src/main/java/com/juliana/vendedores/controller/SellerController.java
+++ src/main/java/com/juliana/vendedores/controller/SellerController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,7 @@ import com.juliana.vendedores.service.SellerService;
 import jakarta.validation.Valid;
 
 @RestController
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/sellers") //caminho base da api
 public class SellerController {
 
